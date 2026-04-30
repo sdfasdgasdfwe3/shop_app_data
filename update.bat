ca5e3b4667a467c937c2e4a5f1fc4358ddb78b0c
sdfasdgasdfwe3 <a_salimyanov@bk.ru>
2026-05-01 04:51:12 +0500

    Auto-update database and code: 01.05.2026  4:51:12,34

--- update.bat
+++ update.bat
@@ -17,17 +17,6 @@ git commit -m "Auto-update database and code: %date% %time%"
 if %errorlevel% neq 0 (
     echo.
     echo No new changes found to commit. Checking for pending commits to push...
-<<<<<<< HEAD
-)
-
-rem Pull latest changes from GitHub to avoid conflicts
-git pull origin main --rebase
-if %errorlevel% neq 0 (
-    echo.
-    echo Error: Failed to pull data from GitHub. Please resolve conflicts manually.
-    goto end
-=======
->>>>>>> 667f478 (Auto-update database and code: 01.05.2026  4:43:58,27)
 )
 
 rem Pull latest changes from GitHub to avoid conflicts
